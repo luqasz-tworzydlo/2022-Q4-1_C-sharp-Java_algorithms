65aa45ca59ab53fdbe74469ed6e5a67097dc02c5
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2022-12-04 02:42:12 +0100

    Add files via upload

---
+++ 4.1_e6-dijkstra's_algorithm/src/e6-dijkstra's_algorithm/e6-dijkstra's_algorithm_c-sharp/e6-dijkstra's_algorithm/1_Program.cs
@@ -0,0 +1,35 @@
+﻿using e6_dijkstra_s_algorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e6_dijkstra_s_algorithm
+{
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Łukasz Tworzydło - nr albumu: gd29623 [zadanie z Algorytmu Dijkstry z 21.11.2022]
+    // Informatyka, grupa laboratoryjna: INiS3_PR2.2 [Algorytmy i struktury danych]
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] graph = new int[,]
+            {
+            {0, 10, 0, 30, 100 },
+            {0, 0, 50, 0, 0 },
+            {0, 0, 0, 0, 10},
+            {0, 0, 20, 0, 60},
+            {0, 0, 0, 0, 0}
+            };
+
+            GFG t = new GFG();
+            t.dijkstra(graph, 0);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4.1_e6-dijkstra's_algorithm/src/e6-dijkstra's_algorithm/e6-dijkstra's_algorithm_c-sharp/e6-dijkstra's_algorithm/3_This_Vertex.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e6_dijkstra_s_algorithm
+{
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Łukasz Tworzydło - nr albumu: gd29623 [zadanie z Algorytmu Dijkstry z 21.11.2022]
+    // Informatyka, grupa laboratoryjna: INiS3_PR2.2 [Algorytmy i struktury danych]
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    internal class This_Vertex
+    {
+        public String name;
+        public int
+            status,
+            predecessor,
+            pathLength;
+
+        public This_Vertex(String name)
+        {
+            this.name = name;
+        }
+    }
+}
