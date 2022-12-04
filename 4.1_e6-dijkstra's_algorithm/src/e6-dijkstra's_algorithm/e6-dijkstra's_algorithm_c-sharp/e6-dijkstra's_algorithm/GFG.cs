330a93c0c996a992e7a7f784062e2bcdf9295d43
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2022-12-04 02:38:48 +0100

    Add files via upload

--- 4.1_e6-dijkstra's_algorithm/src/e6-dijkstra's_algorithm/e6-dijkstra's_algorithm_c-sharp/e6-dijkstra's_algorithm/GFG.cs
+++ 4.1_e6-dijkstra's_algorithm/src/e6-dijkstra's_algorithm/e6-dijkstra's_algorithm_c-sharp/e6-dijkstra's_algorithm/GFG.cs
@@ -39,38 +39,38 @@ namespace e6_dijkstra_s_algorithm
             for (int i = 0; i < Max_Amount_of_Vertices; i++)
                 Console.Write(i + " \t\t " + dist[i] + "\n");
 
-            // 'to do' ścieżka od 0 do 4 [sekwencja tych wierzchołków, od 1 do 5]
+            // 'to do' => ścieżka od 0 do 4 [sekwencja tych wierzchołków, od 1 do 5]
 
-            GFG g = new GFG();
-            g.InsertVertex("Zero");
-            g.InsertVertex("One");
-            g.InsertVertex("Two");
-            g.InsertVertex("Three");
-            g.InsertVertex("Four");
+            GFG D_SPL = new GFG();
+            D_SPL.InsertVertex("Zero");
+            D_SPL.InsertVertex("One");
+            D_SPL.InsertVertex("Two");
+            D_SPL.InsertVertex("Three");
+            D_SPL.InsertVertex("Four");
 
-            g.InsertEdge("Zero", "One", 10);
-            g.InsertEdge("Zero", "Four", 100);
-            g.InsertEdge("Zero", "Three", 30);
-            g.InsertEdge("One", "Two", 50);
-            g.InsertEdge("Two", "Four", 10);
-            g.InsertEdge("Three", "Two", 20);
-            g.InsertEdge("Three", "Four", 60);
+            D_SPL.InsertEdge("Zero", "One", 10);
+            D_SPL.InsertEdge("Zero", "Four", 100);
+            D_SPL.InsertEdge("Zero", "Three", 30);
+            D_SPL.InsertEdge("One", "Two", 50);
+            D_SPL.InsertEdge("Two", "Four", 10);
+            D_SPL.InsertEdge("Three", "Two", 20);
+            D_SPL.InsertEdge("Three", "Four", 60);
 
-            g.FindPaths("Zero");
+            D_SPL.FindPaths("Zero");
         }
 
         /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///
         /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///
+        /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///
 
-        int n;
-        int e;
+        int n, e;
         int[,] adj;
         Vertex[] vertexList;
 
-        private readonly int TEMPORARY = 1;
-        private readonly int PERMANENT = 2;
-        private readonly int NIL = -1;
-        private readonly int INFINITY = 99999;
+        private readonly int Temporary_Value = 1;
+        private readonly int Permanent_Value = 2;
+        private readonly int Nil_Value = -1;
+        private readonly int Infinity_Value = 99999;
 
         public GFG()
         {
@@ -79,31 +79,29 @@ namespace e6_dijkstra_s_algorithm
         }
 
 
-        private void Dijkstra(int s)
+        private void dijkstra_plus(int s)
         {
             int v, c;
 
             for (v = 0; v < n; v++)
             {
-                vertexList[v].status = TEMPORARY;
-                vertexList[v].pathLength = INFINITY;
-                vertexList[v].predecessor = NIL;
+                vertexList[v].status = Temporary_Value;
+                vertexList[v].pathLength = Infinity_Value;
+                vertexList[v].predecessor = Nil_Value;
             }
 
             vertexList[s].pathLength = 0;
 
             while (true)
             {
-                c = TempVertexMinPL();
+                c = TempVertexMinPathLength();
+                if (c == Nil_Value) return;
 
-                if (c == NIL)
-                    return;
-
-                vertexList[c].status = PERMANENT;
+                vertexList[c].status = Permanent_Value;
 
                 for (v = 0; v < n; v++)
                 {
-                    if (IsAdjacent(c, v) && vertexList[v].status == TEMPORARY)
+                    if (IsAdjacent(c, v) && vertexList[v].status == Temporary_Value)
                         if (vertexList[c].pathLength + adj[c, v] < vertexList[v].pathLength)
                         {
                             vertexList[v].predecessor = c;
@@ -113,55 +111,36 @@ namespace e6_dijkstra_s_algorithm
             }
         }
 
-        private int TempVertexMinPL()
+        private int TempVertexMinPathLength()
         {
-            int min = INFINITY;
-            int x = NIL;
+            int min = Infinity_Value, x = Nil_Value;
             for (int v = 0; v < n; v++)
             {
-                if (vertexList[v].status == TEMPORARY && vertexList[v].pathLength < min)
-                {
-                    min = vertexList[v].pathLength;
-                    x = v;
-                }
+                if (vertexList[v].status == Temporary_Value && vertexList[v].pathLength < min)
+                { min = vertexList[v].pathLength; x = v; }
             }
             return x;
         }
 
-        public void FindPaths(String source)
+        public void FindPaths(String src)
         {
-            int s = GetIndex(source);
+            int s = GetIndex(src); dijkstra_plus(s);
+            Console.WriteLine("\n=> Source Vertex: " + src + "\n");
 
-            Dijkstra(s);
-
-            Console.WriteLine("\nSource Vertex: " + source + "\n");
-
-            // for each destination vertex [!!!]
-            /*for (int v = 0; v < n; v++)
-            {
-                Console.WriteLine("Destination Vertex : " + vertexList[v].name);
-                if (vertexList[v].pathLength == INFINITY)
-                    Console.WriteLine("There is no path from " + source + " to vertex " + vertexList[v].name + "\n");
-                else
-                    FindPath(s, v);
-            }*/
-
-            // selected destination, v = 4
+            // selected destination, v = 4; (!) change v = 4 to v = 0 if you want to see pats for each destination vertex (!)
             for (int v = 4; v < n; v++)
             {
                 Console.WriteLine("=> Destination Vertex: " + vertexList[v].name);
-                if (vertexList[v].pathLength == INFINITY)
-                    Console.WriteLine("=> There is no path from " + source + " to vertex " + vertexList[v].name + "\n");
-                else
-                    FindPath(s, v);
+                if (vertexList[v].pathLength == Infinity_Value)
+                    Console.WriteLine("=> There is no path from " + src + " to vertex " + vertexList[v].name + "\n");
+                else FindPath(s, v);
             }
         }
 
         private void FindPath(int s, int v)
         {
-            int i, u;
+            int i, u, sd = 0, count = 0;
             int[] path = new int[n];
-            int sd = 0, count = 0;
 
             while (v != s)
             {
@@ -172,107 +151,75 @@ namespace e6_dijkstra_s_algorithm
             count++; path[count] = s;
 
             Console.Write("=> Shortest Path is: ");
-            for (i = count; i >= 1; i--)
-                Console.Write(path[i] + " ");
+            for (i = count; i >= 1; i--) Console.Write(path[i] + " ");
             Console.WriteLine("\n=> Shortest Path Lenght is: " + sd + "\n");
         }
 
         private int GetIndex(String s)
         {
             for (int i = 0; i < n; i++)
-                if (s.Equals(vertexList[i].name))
-                    return i;
+                if (s.Equals(vertexList[i].name)) return i;
             throw new System.InvalidOperationException("=> Invalid Vertex");
         }
 
         public void InsertVertex(String name)
-        {
-            vertexList[n++] = new Vertex(name);
-        }
-
+        { vertexList[n++] = new Vertex(name); }
 
         private bool IsAdjacent(int u, int v)
-        {
-            return (adj[u, v] != 0);
-        }
+        { return (adj[u, v] != 0); }
 
         public void InsertEdge(String s1, String s2, int wt)
         {
-            int u = GetIndex(s1);
-            int v = GetIndex(s2);
-            if (u == v)
-                throw new System.InvalidOperationException("=> Not a valid edge");
-
-            if (adj[u, v] != 0)
-                Console.Write("=> Edge already present");
-            else
-            {
-                adj[u, v] = wt;
-                e++;
-            }
+            int u = GetIndex(s1), v = GetIndex(s2);
+            if (u == v) throw new System.InvalidOperationException("=> Not a valid edge");
+            if (adj[u, v] != 0) Console.Write("=> Edge already present");
+            else { adj[u, v] = wt; e++; }
         }
 
-
         /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///
         /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///
+        /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// ///
 
-        // Function that implements Dijkstra's
-        // single source shortest path algorithm
-        // for a graph represented using adjacency
-        // matrix representation
+        // Function that implements Dijkstra's single source shortest path algorithm
+        // for a graph represented using adjacency matrix representation
         public void dijkstra(int[,] graph, int src)
         {
-            int[] dist = new int[Max_Amount_of_Vertices]; // The output array. dist[i]
-                                     // will hold the shortest
-                                     // distance from src to i
+            int[] dist = new int[Max_Amount_of_Vertices];
+            // The output array. dist[i] will hold
+            // the shortest distance from src to i
 
-            // sptSet[i] will true if vertex
-            // i is included in shortest path
-            // tree or shortest distance from
-            // src to i is finalized
+            // sptSet[i] will true if vertex i is included in shortest path
+            // tree or shortest distance from src to i is finalized
             bool[] sptSet = new bool[Max_Amount_of_Vertices];
 
-            // Initialize all distances as
-            // INFINITE and stpSet[] as false
+            // Initialize all distances as INFINITE and stpSet[] as false
             for (int i = 0; i < Max_Amount_of_Vertices; i++)
             {
                 dist[i] = int.MaxValue;
                 sptSet[i] = false;
             }
 
-            // Distance of source vertex
-            // from itself is always 0
+            // Distance of source vertex from itself is always 0
             dist[src] = 0;
 
             // Find shortest path for all vertices
             for (int count = 0; count < Max_Amount_of_Vertices - 1; count++)
             {
-                // Pick the minimum distance vertex
-                // from the set of vertices not yet
-                // processed. u is always equal to
-                // src in first iteration.
+                // Pick the minimum distance vertex from the set of vertices not yet
+                // processed. u is always equal to src in first iteration.
                 int u = minDistance(dist, sptSet);
 
                 // Mark the picked vertex as processed
                 sptSet[u] = true;
-                //int[] P = new int[V];
-                //P[V] = 0;
 
-                // Update dist value of the adjacent
-                // vertices of the picked vertex.
-
+                // Update dist value of the adjacent vertices of the picked vertex.
                 for (int v = 0; v < Max_Amount_of_Vertices; v++)
-                    // Update dist[v] only if is not in
-                    // sptSet, there is an edge from u
-                    // to v, and total weight of path
-                    // from src to v through u is smaller
-                    // than current value of dist[v]
+                    // Update dist[v] only if is not in sptSet, there is an edge
+                    // from u to v, and total weight of path from src to v
+                    // through u is smaller than current value of dist[v]
                     if (!sptSet[v] && graph[u, v] != 0 &&
                          dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
-                    {
-                        dist[v] = dist[u] + graph[u, v];
-                        //todo P[V] = u;
-                    }
+                    { dist[v] = dist[u] + graph[u, v]; }
             }
 
             // print the constructed distance array
